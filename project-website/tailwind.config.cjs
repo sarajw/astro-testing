20302d3b9ae8b15f984d8e2b68d96ee650ad33f3
SJW <88535714+sarajw@users.noreply.github.com>
2022-04-14 13:34:55 +0200

    incremental updates, sign up button

--- project-website/tailwind.config.cjs
+++ project-website/tailwind.config.cjs
@@ -1,6 +1,13 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}'],
 	theme: {
+		screens: {
+		  '2xs': '355px',
+		  'xs': '475px',
+		  ...defaultTheme.screens,
+		},
 		extend: {},
 	},
 	plugins: [],
